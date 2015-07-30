ba9148b3ef045dff891b42675722a594ccbc1ae2
Tianlu Yuan <tianlutheman@gmail.com>
2015-07-30 16:30:58 -0600

    speedier to just use factorial

--- euler_20.py
+++ euler_20.py
@@ -3,5 +3,6 @@ Find the sum of the digits in the number 100!
 """
 
 def factorial_digit_sum(n=100):
-    return sum(map(int, str(reduce(lambda x,y: int(str(x*y).strip('0')), xrange(1,n+1)))))
+    from math import factorial
+    return sum(map(int, str(factorial(n))))
 
