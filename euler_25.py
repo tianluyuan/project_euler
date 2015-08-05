857613ea28dc7b51fd960e998a65c41b9d8c8fdd
Tianlu Yuan <tianlutheman@gmail.com>
2015-08-04 22:38:22 -0600

    euler_25

---
+++ euler_25.py
@@ -0,0 +1,38 @@
+"""
+The Fibonacci sequence is defined by the recurrence relation:
+
+   Hence the first 12 terms will be:
+
+     F[1] = 1
+     F[2] = 1
+     F[3] = 2
+     F[4] = 3
+     F[5] = 5
+     F[6] = 8
+     F[7] = 13
+     F[8] = 21
+     F[9] = 34
+     F[10] = 55
+     F[11] = 89
+     F[12] = 144
+
+   The 12th term, F[12], is the first term to contain three digits.
+
+   What is the first term in the Fibonacci sequence to contain 1000 digits?
+"""
+from euler_15 import memoize
+
+@memoize
+def fib(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+
+def first_fib_term(ndigits=1000):
+    term = 1
+    while len(str(fib(term))) < ndigits:
+        term += 1
+
+    return term
