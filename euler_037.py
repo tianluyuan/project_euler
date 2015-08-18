3ffeb890e02c7c181780d635b0c715bf8f2463a6
Tianlu Yuan <tianlutheman@gmail.com>
2015-08-18 11:44:29 -0600

    lazy 37

--- euler_037.py
+++ euler_037.py
@@ -9,28 +9,21 @@ left to right and right to left.
 
 NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 """
-from utils import no_evens, is_prime, truncate
+from utils import no_evens, is_prime, truncate, lazy_primes
+from itertools import islice, ifilter, dropwhile
+
+
+def check(num):
+    snum = str(num)
+    return is_prime(int(snum[0])) and is_prime(int(snum[-1])) and no_evens(snum[1:])
+
+
+def truncatable_prime(prime):
+    return all(is_prime(num) for num in truncate(prime))
 
 
 def truncatable_primes():
-    curr = 11
-    nprimes = 0
-    while nprimes < 11:
-        scurr = str(curr)
-        if (scurr[0] != '1' and
-            scurr[-1] != '1' and
-            no_evens(int(scurr[1:]))):
-            flag = True
-            for truncated in truncate(curr):
-                if not is_prime(truncated):
-                    flag = False
-                    break
-
-            if flag:
-                nprimes += 1
-                yield curr
-
-        curr += 2
+    return islice(ifilter(truncatable_prime,  ifilter(check, dropwhile(lambda x: 10 > x, lazy_primes()))), 11)
 
 
 def p37():
