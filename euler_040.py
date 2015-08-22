4067ad58b9c3e565a0aea24bae6ee2a2dd1d7483
Tianlu Yuan <tianlutheman@gmail.com>
2015-08-21 21:30:34 -0600

    cleaned up

--- euler_040.py
+++ euler_040.py
@@ -11,23 +11,27 @@ of the following expression.
 
 d1 x d10 x d100 x d1000 x d10000 x d100000 x d1000000
 """
+from itertools import takewhile, count
+
+
+def step(x):
+    return int(9*10**(x-1)*x)
+
+
+def find_limit(pos):
+   return list(takewhile(lambda x: step(x) < pos, count(0)))[-1]
+
+
 def digit(pos):
     """returns the digit at position for the concatenated irrational
     decimal
     """
-    curr = 0
-    l = 1
-    while True:
-        step = 9*10**(l-1)*l
-        if curr + step > pos:
-            break
-        else:
-            curr += step
-            l += 1
+    limit = find_limit(pos)
+    ndigits = sum(map(step, xrange(limit+1)))
 
-    left = pos - curr
-    (nnums, rem) = divmod(left-1, l)
-    posnum = nnums + 10**(l-1)
+    left = pos - ndigits
+    (nnums, rem) = divmod(left-1, limit+1)
+    posnum = nnums + 10**(limit)
     return int(str(posnum)[rem])
 
 
