93f878579bf5bef897ec3c6f3856db13138b7938
Tianlu Yuan <tianlutheman@gmail.com>
2015-09-02 23:32:08 -0600

    p48

---
+++ euler_048.py
@@ -0,0 +1,15 @@
+"""
+The series, 1**1 + 2**2 + 3**3 + ... + 10**10 = 10405071317.
+
+Find the last ten digits of the series, 1**1 + 2**2 + 3**3 + ... + 1000**1000.
+"""
+from itertools import imap
+
+
+def big_sum(upto):
+    return sum((n**n for n in xrange(1, upto+1)))
+
+
+def p48():
+    return str(big_sum(1000))[-10:]
+
