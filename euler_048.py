49ab072eeab289cd24220e260b71f96b99fc91dd
Tianlu Yuan <tianlutheman@gmail.com>
2015-09-02 23:32:32 -0600

    removed unused import

--- euler_048.py
+++ euler_048.py
@@ -3,9 +3,6 @@ The series, 1**1 + 2**2 + 3**3 + ... + 10**10 = 10405071317.
 
 Find the last ten digits of the series, 1**1 + 2**2 + 3**3 + ... + 1000**1000.
 """
-from itertools import imap
-
-
 def big_sum(upto):
     return sum((n**n for n in xrange(1, upto+1)))
 
